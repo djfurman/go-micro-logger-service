36d0f13ac219131cba2769956d0f5e7e3fa745d1
Daniel Furman <djfurman@gmail.com>
2022-11-27 19:58:54 -0800

    Add logic to connect to MongoDB. Add driver dependency for MongoDB.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRPCPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	// connect to MongoDB
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// Create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "local-blue",
+		Password: "secret",
+	})
+
+	// Connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting: ", err)
+		return nil, err
+	}
+
+	return c, nil
 }
